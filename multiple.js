c38365adf26542f247d91dc9e89f563782d8f6a5
joy0789 <johirulislamj62@gmail.comm>
2026-05-17 21:17:21 +0600

    Practice multiple conditions and logical operators in JavaScript

---
+++ multiple.js
@@ -0,0 +1,15 @@
+const salary = 11000;
+const isBCS = true;
+const height = 68;
+const hasCar = false;
+
+// if(salary > 10000 && height > 65){
+//     console.log('Su------patro');
+// }
+// else{
+//     console.log('onno patro khuji');
+// }
+
+if((salary > 10000 || height >= 65) && isBCS ==  true){
+    console.log("sobai raji");
+}
