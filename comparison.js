4ea04a2ac62482267a27e33afabc05d1621269f5
joy0789 <johirulislamj62@gmail.comm>
2026-05-17 19:44:52 +0600

    Practice JavaScript if else statements

--- comparison.js
+++ comparison.js
@@ -11,3 +11,36 @@
  * 7. and: &&
  * 8. or: ||
  * */ 
+
+// console.log(5 > 10);
+// console.log(5 < 10);
+// console.log(5 == 10);
+// console.log(10 == 10);
+// console.log(15 >= 10);
+// console.log(5 <= 10);
+// console.log(10 <= 10);
+
+// console.log(10 == 10);
+// console.log(10 == 10.1);
+
+// ------------------------
+
+var applePrice = 50;
+var orangePrice = 155;
+
+// console.log(applePrice > orangePrice);
+// console.log(orangePrice < applePrice);
+
+/**
+ * ==
+ * === 
+ */ 
+
+// console.log(10 == '10');
+// console.log(10 === '10');
+
+console.log(120 === 'tomato');
+
+// ---------------------------
+// console.log(10 != '10');
+// console.log(10 !== '10');
---
+++ if.js
@@ -0,0 +1,40 @@
+/**
+ * if you buy food for me
+ *      i will come
+ * 
+ * -----------------------
+ * 
+ * if apple price is less than 300tk
+ *      i will buy
+ * 
+ * --------STRUCTURE--------
+ * if (condition){
+ * 
+ * }
+ */
+
+var applePrice = 250;
+
+if(applePrice < 300){
+    console.log('Give me some apple');
+}
+
+/**
+ *if (condition){
+ *      execute code if the condition is true
+ * }
+ * else {
+ *      execute code if the condition is false
+ * }
+ */ 
+
+// var weight = 40;
+// var weight = 10;
+// var weight = 20;
+
+if(weight <= 20){
+    console.log('I will carry it by myself');
+}
+else{
+    console.log('I will rent a rickshaw');
+}
